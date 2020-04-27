3ba8ccde2d5c596f134a0677e44094c56e090091
杨涵麟 <yanghanlin@yanghanlins-MacBook-Pro.local>
2020-04-27 11:49:41 +0800

    Finish superb test.

--- main.py
+++ main.py
@@ -1,31 +1,48 @@
 from random import *
 from tqdm import *
+import numpy as np
 
 
 def oneshot():
 	total = 10
 	prob = 0.02
-	while not [x for x in filter(lambda x: x <= prob, [random() for _ in range(10)])]:
+	
+	while True:
+		star6cnt = len(list(filter(lambda x: x <= prob, [random() for _ in range(10)])))
+		if star6cnt:
+			break
 		total += 10
 		if total>=50:
 			prob += 0.02
 		if total == 300:
 			break
 			
-	return total
+	return total, star6cnt
 
 def oneshot_super():
+	total, cnt = oneshot()
+	while not len(list(filter(lambda x: x <= 0.35, [random() for _ in range(cnt)]))):
+		tmp, cnt = oneshot()
+		total += tmp
+		
+		if total >= 300:
+			total = 300
+			break
+			
+	return total
 
-	
-def run():
+def run(super=False):
 	result = []
 	for _ in tqdm(range(1000000)):
-		result += [oneshot()]
+		if not super:
+			result += [oneshot()[0]]
+		else:
+			result += [oneshot_super()]
 		
 	return  result
 
 
 if __name__ == "__main__":
-	result = run()
+	result = run(True)
 	#for e in result: print(e)
-	print("aver: ", sum(result)  / len(result))
+	print("aver: %f, median: %d" % (np.mean(result), np.median(result)))
